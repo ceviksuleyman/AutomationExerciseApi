b794299d988e199a18dcea22ca4c7e4ff8ba68d9
cevik <suleymancevik64@gmail.com>
2023-01-19 17:21:16 +0300

    cevik

--- src/test/java/stepDefinitions/RestfulPost.java
+++ src/test/java/stepDefinitions/RestfulPost.java
@@ -13,19 +13,16 @@ import java.io.FileReader;
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
-
 public class RestfulPost {
 
     @Test
     public void post() throws FileNotFoundException {
-
 
         File file = new File("src/test/resources/TestData/RestfulPostBody.json");
         FileReader fileReader = new FileReader(file);
         JSONTokener jsonTokener = new JSONTokener(fileReader);
         JSONObject expectedDataJSONObject = new JSONObject(jsonTokener);
         System.out.println("expectedDataJSONObject = " + expectedDataJSONObject);
-
 
         Response response = given().pathParam("1", "booking")
                 .contentType(ContentType.JSON)
@@ -34,20 +31,22 @@ public class RestfulPost {
 
         response.prettyPrint();
 
-
         // JSONObject
         JSONObject actualJSONObject = new JSONObject(response.asString());
         System.out.println("ActualJSONObject = " + actualJSONObject);
 
-
         // Do Assertion
         // booking
-        assertEquals(expectedDataJSONObject.getString("firstname"), actualJSONObject.getJSONObject("booking").getString("firstname"));
-        assertEquals(expectedDataJSONObject.getString("lastname"), actualJSONObject.getJSONObject("booking").getString("lastname"));
-        assertEquals(expectedDataJSONObject.getInt("totalprice"), actualJSONObject.getJSONObject("booking").getInt("totalprice"));
-        assertEquals(expectedDataJSONObject.getBoolean("depositpaid"), actualJSONObject.getJSONObject("booking").getBoolean("depositpaid"));
+        assertEquals(expectedDataJSONObject.getString("firstname"),
+                actualJSONObject.getJSONObject("booking").getString("firstname"));
+        assertEquals(expectedDataJSONObject.getString("lastname"),
+                actualJSONObject.getJSONObject("booking").getString("lastname"));
+        assertEquals(expectedDataJSONObject.getInt("totalprice"),
+                actualJSONObject.getJSONObject("booking").getInt("totalprice"));
+        assertEquals(expectedDataJSONObject.getBoolean("depositpaid"),
+                actualJSONObject.getJSONObject("booking").getBoolean("depositpaid"));
 
-        //bookingdates
+        // bookingdates
         assertEquals(expectedDataJSONObject.getJSONObject("bookingdates").getString("checkin"),
                 actualJSONObject.getJSONObject("booking").getJSONObject("bookingdates").getString("checkin"));
 
